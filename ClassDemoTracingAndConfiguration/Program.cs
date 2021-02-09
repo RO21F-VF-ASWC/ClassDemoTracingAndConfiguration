6a6426aeddc18f8bce9d69085bb451594f44171d
Peter Levinsky <pele@easj.dk>
2021-02-09 13:02:39 +0100

    Nu med konfigurering

--- ClassDemoTracingAndConfiguration/Program.cs
+++ ClassDemoTracingAndConfiguration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace ClassDemoTracingAndConfiguration
 {
@@ -9,13 +10,30 @@ namespace ClassDemoTracingAndConfiguration
             ReadConfiguration();
 
             TraceWorker worker = new TraceWorker();
-            worker.Start();
+            //worker.Start();
         }
 
         private static void ReadConfiguration()
         {
-            
+            XmlDocument configDoc = new XmlDocument(); 
+            configDoc.Load(@"M:\uv\2021f-AvanceretCsharp\prog\Data\ServerConfiguration.conf");
 
+            XmlNode portNode = configDoc.DocumentElement.SelectSingleNode("Port"); 
+            if (portNode != null) { 
+                String portStr = portNode.InnerText.Trim(); 
+                int serverPort = Convert.ToInt32(portStr);
+
+
+                Console.WriteLine("Server port er " + serverPort);
+            }
+
+            XmlNode nameNode = configDoc.DocumentElement.SelectSingleNode("Name");
+            if (nameNode != null)
+            {
+                String nameStr = nameNode.InnerText.Trim();
+                
+            Console.WriteLine("Server name er " + nameStr);
+            }
         }
     }
 }
