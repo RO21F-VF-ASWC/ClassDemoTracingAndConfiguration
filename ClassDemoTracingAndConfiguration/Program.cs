3f521e741ff8b85c6adb4633f695a244773c4fce
Peter Levinsky <pele@easj.dk>
2021-02-09 10:34:00 +0100

    Add project files.

---
+++ ClassDemoTracingAndConfiguration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassDemoTracingAndConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadConfiguration();
+
+            TraceWorker worker = new TraceWorker();
+            worker.Start();
+        }
+
+        private static void ReadConfiguration()
+        {
+            
+
+        }
+    }
+}
---
+++ ClassDemoTracingAndConfiguration/TraceWorker.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ClassDemoTracingAndConfiguration
+{
+    public class TraceWorker
+    {
+        private TraceSource ts = new TraceSource("Peters Demo");
+        public TraceWorker()
+        {
+            ts.Switch = new SourceSwitch("Peter","All");
+
+            TraceListener consoleLog = new ConsoleTraceListener();
+            ts.Listeners.Add(consoleLog);
+
+            TraceListener fileLog = new TextWriterTraceListener( new StreamWriter("TraceDemo.txt"));
+            ts.Listeners.Add(fileLog);
+            //fileLog.Filter = new EventTypeFilter(SourceLevels.Error);
+
+            TraceListener eventLog = new EventLogTraceListener("Application");
+            //ts.Listeners.Add(eventLog);
+            eventLog.Filter = new EventTypeFilter(SourceLevels.Error);
+        }
+
+        public void Start()
+        {
+            ts.TraceEvent(TraceEventType.Information, 333, "dette er information");
+            ts.TraceEvent(TraceEventType.Warning, 333, "dette er warning");
+            ts.TraceEvent(TraceEventType.Error, 333, "dette er error");
+            ts.TraceEvent(TraceEventType.Critical, 333, "dette er critical");
+            
+
+            ts.Close();
+        }
+    }
+}
